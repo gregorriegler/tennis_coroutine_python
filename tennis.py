24cbbcac91abda65df5ef403c96f380afc9de6b1
Gregor Riegler <gregor.riegler@ebcont.com>
2019-07-28 20:08:45 +0200

    trying

--- tennis.py
+++ tennis.py
@@ -5,28 +5,64 @@ def point_name(point):
     return ["0", "15", "30", "40"][point]
 
 
-def score_as_string(score):
-    if score[0] >= 3 and score[1] >= 3:
-        if score[0] == score[1]:
-            return "deuce"
-        elif 2 > abs(score[0] - score[1]) > 0:
-            if score[0] > score[1]:
-                return "advantage player 1"
-            else:
-                return "advantage player 2"
+def round_score(score):
+    return "(" + str(score[0]) + ":" + str(score[1]) + ")"
+
+
+def simple_score(score, overall_score):
+    return point_name(score[0]) + "-" + point_name(score[1]) + " " + round_score(overall_score)
+
+
+def deuce_score(score, overall_score):
+    if score[0] == score[1]:
+        return "deuce " + round_score(overall_score)
+    else:
+        if score[0] > score[1]:
+            return "advantage player 1 " + round_score(overall_score)
         else:
-            if score[0] > score[1]:
-                return "player 1 won"
-            else:
-                return "player 2 won"
-    elif score[0] <= 3 and score[1] <= 3:
-        return point_name(score[0]) + "-" + point_name(score[1])
+            return "advantage player 2 " + round_score(overall_score)
+
+
+def won_score(score, overall_score):
+    if score[0] > score[1]:
+        return "player 1 won " + round_score(overall_score)
+    else:
+        return "player 2 won " + round_score(overall_score)
 
 
 @coroutine
-def game(board):
-    score = [0, 0]
+def game(scoreboard):
+    overall_score = [0, 0]
     while True:
-        board(score_as_string(score))
-        who_scored = (yield) - 1
-        score[who_scored] = score[who_scored] + 1
+        sentence_score = [0, 0]
+
+        scoreboard(simple_score(sentence_score, overall_score))
+
+        while True:
+            scorer = yield from get_scorer()
+            increase_score(sentence_score, scorer)
+
+            if sentence_won(sentence_score):
+                scoreboard(won_score(sentence_score, overall_score))
+                increase_score(overall_score, scorer)
+                break
+            elif in_deuce(sentence_score):
+                scoreboard(deuce_score(sentence_score, overall_score))
+            else:
+                scoreboard(simple_score(sentence_score, overall_score))
+
+
+def increase_score(score, who_scored):
+    score[who_scored] = score[who_scored] + 1
+
+
+def get_scorer():
+    return (yield) - 1
+
+
+def in_deuce(score):
+    return score[0] >= 3 and score[1] >= 3
+
+
+def sentence_won(score):
+    return (score[0] > 3 or score[1] > 3) and abs(score[0] - score[1]) > 1
--- test.py
+++ test.py
@@ -3,27 +3,85 @@ from tennis import game
 
 
 class TestTennis(unittest.TestCase):
-    expected_score = """0-0
-15-0
-30-0
-40-0
-40-15
-40-30
-deuce
-advantage player 1
-deuce
-advantage player 2
-deuce
-advantage player 1
-player 1 won
-"""
-
     actual_score = ""
+
+    def setUp(self):
+        self.actual_score = "";
 
     def board(self, score):
         self.actual_score += score + "\n"
 
-    def test_tennis(self):
+    def test_tennis_player1_win_a_sentence(self):
+        expected_score = """0-0 (0:0)
+15-0 (0:0)
+30-0 (0:0)
+40-0 (0:0)
+player 1 won (0:0)
+0-0 (1:0)
+"""
+        g = game(self.board)
+        g.send(1)
+        g.send(1)
+        g.send(1)
+        g.send(1)
+        self.assertEqual(expected_score, self.actual_score)
+
+    def test_tennis_player2_win_a_sentence(self):
+        expected_score = """0-0 (0:0)
+0-15 (0:0)
+0-30 (0:0)
+0-40 (0:0)
+player 2 won (0:0)
+0-0 (0:1)
+"""
+        g = game(self.board)
+        g.send(2)
+        g.send(2)
+        g.send(2)
+        g.send(2)
+        self.assertEqual(expected_score, self.actual_score)
+
+    def test_tennis_players_go_1_1(self):
+        expected_score = """0-0 (0:0)
+15-0 (0:0)
+30-0 (0:0)
+40-0 (0:0)
+player 1 won (0:0)
+0-0 (1:0)
+0-15 (1:0)
+0-30 (1:0)
+0-40 (1:0)
+player 2 won (1:0)
+0-0 (1:1)
+"""
+        g = game(self.board)
+        g.send(1)
+        g.send(1)
+        g.send(1)
+        g.send(1)
+        g.send(2)
+        g.send(2)
+        g.send(2)
+        g.send(2)
+        self.assertEqual(expected_score, self.actual_score)
+
+    def test_tennis_long_game(self):
+        expected_score = """0-0 (0:0)
+15-0 (0:0)
+30-0 (0:0)
+40-0 (0:0)
+40-15 (0:0)
+40-30 (0:0)
+deuce (0:0)
+advantage player 1 (0:0)
+deuce (0:0)
+advantage player 2 (0:0)
+deuce (0:0)
+advantage player 1 (0:0)
+player 1 won (0:0)
+0-0 (1:0)
+"""
+
         g = game(self.board)
         g.send(1)
         g.send(1)
@@ -37,7 +95,7 @@ player 1 won
         g.send(1)
         g.send(1)
         g.send(1)
-        self.assertEqual(self.actual_score, self.expected_score)
+        self.assertEqual(expected_score, self.actual_score)
 
 
 if __name__ == '__main__':
